ed2f233630988668fb42177cfb187287dfc2b218
Alex Pessoa <alexpessoajr@gmail.com>
2022-02-23 07:58:30 -0300

    Update DiceRolling.kt

--- Codeforces/DiceRolling.kt
+++ Codeforces/DiceRolling.kt
@@ -1,18 +1,8 @@
 
-fun main () {
-  val n = readLine()!!.toInt()
-
-  for (i in (0 until n)) {
-    val x = readLine()!!.toInt()
-    var ct: Int
-
-    if (x % 2 == 0) {
-      ct = x / 2
-    } else {
-      ct = (x - 3) / 2 + 1
-    }
-
-    println(ct)
+fun main() {
+  for (i in (0 until readln().toInt())) {
+    val x = readln().toInt()
+    println(if (x % 2 == 0) x / 2 else (x - 3) / 2 + 1)
   }
 }
 
