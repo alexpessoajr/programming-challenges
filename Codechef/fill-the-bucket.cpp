dfcfc1273f7ace0a40862829414999d82ef4cc5d
Alex Pessoa <alexpessoajr@gmail.com>
2022-03-13 16:17:06 -0300

    Add fill-the-bucket.cpp

---
+++ Codechef/fill-the-bucket.cpp
@@ -0,0 +1,17 @@
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  int t, x, k;
+  
+  cin >> t;
+
+  for (int i = 0; i < t; i++) {
+    cin >> k >> x;
+    cout << k - x << endl;
+  }
+
+  return 0;
+}
