d123a796a0e55aa4b2a673116395498d0b2b1925
Alex Pessoa <alexpessoajr@gmail.com>
2022-03-17 20:14:30 -0300

    Add template.cpp

---
+++ template.cpp
@@ -0,0 +1,26 @@
+
+#include <bits/stdc++.h>
+
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+
+#define REP(i,a,b) for (int i = a; i <= b; i++)
+#define REPEAT(times) for (int it = 1; it <= times; it++)
+
+#define INF 0x3f3f3f3f
+#define EPS 1e-9
+
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int, int> pii;
+
+int main() {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+
+  return 0;
+}
