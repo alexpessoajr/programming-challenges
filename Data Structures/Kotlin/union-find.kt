141e3cc51c67c91ecfc561b264f3ae4a7afb0236
Alex Pessoa <alexpessoajr@gmail.com>
2022-03-18 02:00:40 -0300

    Add union-find.kt

---
+++ Data Structures/Kotlin/union-find.kt
@@ -0,0 +1,32 @@
+
+class UnionFind {
+  private val parent: Array<Int>
+  private val size: Array<Int>
+
+  constructor(size: Int) {
+    this.parent = Array<Int>(size + 1) { it }
+    this.size = Array(size + 1) { 1 }
+  }
+
+  fun connect(a: Int, b: Int) {
+    var x = find(a)
+    var y = find(b)
+    if (x != y) {
+      if (size[x] > size[y]) {
+        x = y.also { y = x }
+      }
+      parent[x] = y
+      size[y] += size[x]
+    }
+  }
+
+  fun isConnected(a: Int, b: Int): Boolean = find(a) == find(b)
+
+  private fun find(x: Int): Int = if (parent[x] == x) x else {
+    find(parent[x]).let {
+      it.also {
+        parent[x] = it
+      }
+    }
+  }
+}
